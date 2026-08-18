0d005e2c936705edb8a87258da98487ef76fa390
Kunmitech1 <olumuyiwa761@gmail.com>
2026-08-18 11:25:19 +0100

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -6,4 +6,4 @@ const SUPABASE_ANON_KEY = 'sb_publishable_XFEZspknxmRdcr5dixRl2Q_QSbPSwVo';
 
 // Creates one shared client both apps use.
 
-const supabase = window.supabase.createClient(https://lrocighnjtundvcxvhar.supabase.co, eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyb2NpZ2huanR1bmR2Y3h2aGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMzk0MTQsImV4cCI6MjEwMjYxNTQxNH0.w8fmLmbs6M4HMUG5e--eSpNynLs3BDSvwT8ZIZR9i50);
+const supabase = window.supabase.createClient(https://lrocighnjtundvcxvhar.supabase.co, sb_publishable_XFEZspknxmRdcr5dixRl2Q_QSbPSwVo);
