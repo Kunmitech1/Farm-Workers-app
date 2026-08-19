b9972b35fba3e723484a8ef3ac830024c865d29c
Kunmitech1 <olumuyiwa761@gmail.com>
2026-08-19 03:59:21 +0100

    Delete supabase-config.js

--- supabase-config.js
+++
@@ -1,3 +0,0 @@
-const SUPABASE_URL = 'https://lrocighnjtundvcxvhar.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxyb2NpZ2huanR1bmR2Y3h2aGFyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMzk0MTQsImV4cCI6MjEwMjYxNTQxNH0.w8fmLmbs6M4HMUG5e--eSpNynLs3BDSvwT8ZIZR9i50';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
